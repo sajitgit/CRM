8cd832e1376005204a0fcee043648e22ba86ca38
hp <hp@DESKTOP-8QRD8C9.bbrouter>
2024-12-30 18:31:00 +0530

    wait

--- QALegend/src/main/java/Constants/ConstantValues.java
+++ QALegend/src/main/java/Constants/ConstantValues.java
@@ -3,7 +3,7 @@ package Constants;
 public class ConstantValues {
 
 	public static final String TESTDATAFILE = System.getProperty("user.dir")+"\\src\\test\\resources\\ExcelData.xlsx";
-	public static final int EXPLICITWAIT=30;
+	public static final int EXPLICITWAIT=20;
 	public static final String USERDATAFILE=System.getProperty("user.dir")+"\\src\\test\\resources\\userdata.properties";
 	
 }
